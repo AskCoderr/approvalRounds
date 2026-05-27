5d95e8a44a41c5627f80250be55905fb7303f88b
Ashwin S Krishna <ashwinskrishna2006@gmail.com>
2026-05-27 18:24:36 +0530

    feat(ui): Add create-workspace button
    Add a button at the top of the container with position sticky that on click, will display (for now) the overlay div

--- node/public/index.js
+++ node/public/index.js
@@ -2,11 +2,14 @@ const container = document.querySelector('.container');
 const overlay = document.querySelector('.overlay');
 const workspaceEditMenu = document.querySelector('.workspace-edit-menu');
 const closeForms = document.querySelector('.close-forms');
+const addWorkspaceButton = document.querySelector('.add-workspace-button');
 
 container.addEventListener('click', (event) => {
     if (event.target.matches('.edit-btn, .edit-icon')) {
         overlay.style.display = 'block';
         workspaceEditMenu.style.display = 'block';
+    } else if (event.target.matches('.add-workspace-button')) {
+        overlay.style.display = 'block';
     }
 });
 
